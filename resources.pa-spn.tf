dc57dc01f019c739e98fe74ff26c4bfc63ad12e5
Florian Nowarre <fnowarre@meshcloud.io>
2024-12-12 16:54:21 +0100

    remove directory stuff

--- resources.pa-spn.tf
+++ resources.pa-spn.tf
@@ -1,4 +1,4 @@
-resource "azurerm_role_definition" "buildingblock_plan" {
+resource "azurerm_role_definition" "paloalto" {
   name        = "${var.service_principal_name}-panorama"
   description = "the role definition is for connecting panorma to the ngfw"
   scope       = var.scope
@@ -16,8 +16,8 @@ resource "azurerm_role_definition" "buildingblock_plan" {
   }
 }
 
-resource "azurerm_role_assignment" "palo_alto" {
-  role_definition_id = azurerm_role_definition.buildingblock_plan.role_definition_resource_id
+resource "azurerm_role_assignment" "paloalto" {
+  role_definition_id = azurerm_role_definition.paloalto.role_definition_resource_id
   principal_id       = azuread_service_principal.spn_paloalto.object_id
   scope              = var.scope
 }
@@ -36,14 +36,6 @@ data "azuread_application_published_app_ids" "well_known" {}
 
 data "azuread_service_principal" "msgraph" {
   client_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
-}
-
-# allow reading groups and users
-resource "azuread_app_role_assignment" "spn_paloalto-directory" {
-  app_role_id        = data.azuread_service_principal.msgraph.app_role_ids["Directory.Read.All"]
-  resource_object_id = data.azuread_service_principal.msgraph.object_id
-
-  principal_object_id = azuread_service_principal.spn_paloalto.object_id
 }
 
 # note this rotation technique requires the terraform to be run regularly
